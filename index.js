b70b43b740a67ba5d385edabe5b07e8dc15663a1
ar00gula <arianna.mayer@gmail.com>
2021-01-18 13:01:48 -0500

    commit tree 58f5000901005f259c58e31e161aa5a1c710d2c4

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+
+function findMatching(driversArray, string) {
+    return driversArray.filter(function(driver) {
+        return driver.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(driversArray, string) {
+    return driversArray.filter(driver => {
+        return driver[0].toLowerCase() === string[0].toLowerCase()
+    })
+}
+
+function matchName(driversArray, string) {
+    return driversArray.filter(function(driver) {
+        return driver.name === string
+    })
+}
